455af289c1bf6191e347ebd4ff75fd70e7517b1e
Maciejszk <maciek.szymkiewicz1@gmail.com>
2023-10-19 11:56:34 +0200

    part 1.5

---
+++ Part1/src/App.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+// Header component
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  );
+}
+
+// Content component
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+}
+
+// Part component (used by Content component)
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+}
+
+// Total component
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <p>Number of exercises {totalExercises}</p>
+  );
+}
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+    name: 'Using props to pass data', 
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+}
+export default App;
